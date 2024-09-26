86be91989f6f4acfafe4a3e682366eaff7fa0588
kiran12kuhrade <Github@1999>
2024-09-26 15:56:44 +0530

    [Kiran] update:Added Author ,Desc,Date

--- src/main/java/com/Employee/LineComparisonComputation.java
+++ src/main/java/com/Employee/LineComparisonComputation.java
@@ -1,8 +1,13 @@
 package com.Employee;
 
+/*
+Author:Kiran Kisan Kurhade
+Desc:Welcome Message Only
+Date:26/09/2024
+ */
 public class LineComparisonComputation {
     public static void main(String[] args) {
 
-        System.out.println("https://github.com/kiran12kurhade/LineComparisonComputation.git");
+        System.out.println("Welcome to Line Comparison Computation Program");
     }
 }
